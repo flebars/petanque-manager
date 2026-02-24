99e5c1bdd0cbf65383190cf4510486ff78172f48
frederic.lebars@itesoft.com <frederic.lebars@itesoft.com>
2026-02-24 09:57:54 +0100

    debug: log actual PORT used by nginx at startup

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 PORT=${PORT:-3000}
+echo "Starting nginx on port $PORT"
 sed "s/__PORT__/${PORT}/g" /etc/nginx/nginx.conf.template > /etc/nginx/conf.d/default.conf
 exec nginx -g "daemon off;"
